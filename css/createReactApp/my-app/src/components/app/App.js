7c1c4f3989b9d52a3306cd6881e60d38db1fca2f
unknown <mathiasimagine@gmail.com>
2021-12-31 13:39:48 -0300

    Ravenous

--- css/createReactApp/my-app/src/components/app/App.js
+++ css/createReactApp/my-app/src/components/app/App.js
@@ -4,13 +4,36 @@ import './App.css';
 import BusinessList from "./Business/BusinessList/BusinessList";
 import SearchBar from './SearchBar/SearchBar';
 
+
+
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+
+const businesses=[business,business,business,business,business,business] 
 class App extends Component{
+  constructor(){
+    super();
+
+  }
+  searchYelp(tems,location,sortBy){
+    console.log(`Searching ${tems} with Pizza, ${location}, ${sortBy}`)
+  }
   render(){
     return(
       <div className="App">
         <h1>Ravenous</h1>
-        <SearchBar />
-        <BusinessList />
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses ={businesses}/>
       </div>
     )
   }
--- css/createReactApp/my-app/src/components/app/SearchBar/SearchBar.js
+++ css/createReactApp/my-app/src/components/app/SearchBar/SearchBar.js
@@ -2,40 +2,87 @@ import React from "react";
 import "./SearchBar.css";
 
 
-
-const sortByOptions={
-    "bestMatch":"best_match", // key: "value",
-    "hightestRated":"rating",
-    "mostReviewed":"review_count"
-}
- // Objet.keys => [bestMatch,hightestRated,mostReviewed]
- // [bestMatch,hightestRated,mostReviewed].map(cb)
+// Objet.keys => [bestMatch,hightestRated,mostReviewed]
+// [bestMatch,hightestRated,mostReviewed].map(cb)
 //                            i
-export default class SearchBar extends React.Component{
-    renderSortByOptions(){
-        return Object.keys(sortByOptions).map(sortByOption=>{ let sortByOptionValue= sortByOptions[sortByOption];
-        return <li key={sortByOptionValue}> {sortByOption} </li>
-        })
-        // key es el identificador unico que pide React para componentes hijos
+const sortByOptions = {
+  "bestMatch": "best_match", // key: "value",
+  "hightestRated": "rating",
+  "mostReviewed": "review_count"
+}
+export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      location: "",
+      sortBy: "best_match"
     }
-    // [bestMatch,hightestRated,mostReviewed].map(elemt => 
-    // let value=sortByOptions[elemt] )
-    render(){
-        return(
-            <div className="SearchBar">
-  <div className="SearchBar-sort-options">
-    <ul>
-        {this.renderSortByOptions()}
-    </ul>
-  </div>
-  <div className="SearchBar-fields">
-    <input placeholder="Search Businesses" />
-    <input placeholder="Where?" />
-  </div>
-  <div className="SearchBar-submit">
-    <a>Let's Go</a>
-  </div>
-</div>
-        )
+
+    this.handleTermsChange=this.handleTermsChange.bind(this);
+    this.handleLocationChange=this.handleLocationChange.bind(this);
+    this.handleSearch=this.handleSearch.bind(this);
+  }
+  getSortByClass(sortByOption){
+    if(sortByOption===this.state.sortBy){
+      return"active"
     }
+    return "";
+  }
+
+  handleTermsChange(e){
+    this.setState({term: e.target.value})
+  }
+
+  handleLocationChange(e){
+    this.setState({location: e.target.value})
+  }
+
+  handleSearch(e){
+    this.props.searchYelp(this.state.term,this.state.location,this.state.sortBy);
+    e.preventDefault();
+
+  }
+
+  handleSortByChange(sortByOption){
+      this.setState({sortBy:sortByOption})
+  }
+
+  renderSortByOptions() {
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li
+                key={sortByOptionValue}
+                className={this.getSortByClass(sortByOptionValue)}
+                onClick={this.handleSortByChange.bind(this,sortByOptionValue)}
+               >{sortByOption}</li>
+    })
+    // key es el identificador unico que pide React para componentes hijos
+  }
+  // [bestMatch,hightestRated,mostReviewed].map(elemt => 
+  // let value=sortByOptions[elemt] )
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()}
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input 
+          placeholder="Search Businesses"
+          onChange={this.handleTermsChange}
+           />
+          <input
+           placeholder="Where?"
+           onChange={this.handleLocationChange}
+            />
+        </div>
+        <div className="SearchBar-submit">
+          <a onClick={this.handleSearch}>Let's Go</a>
+        </div>
+      </div>
+    )
+  }
 }
